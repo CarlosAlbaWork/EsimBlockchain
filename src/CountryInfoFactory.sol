3acbd74f6cf055ee06f3ce7523fc5eadfe47f676
Carlos Alba <carlosalbawork@gmail.com>
2024-07-03 12:04:04 +0200

    Natspec CountryInfoFactory

--- src/CountryInfoFactory.sol
+++ src/CountryInfoFactory.sol
@@ -17,16 +17,35 @@ contract CountryInfoFactory is Ownable {
 
     constructor() Ownable(msg.sender) {}
 
+    /**
+     * @dev Struct that contains the information of the phones
+     * countryOfPhoneNumber : ID of the country where the phone number is from
+     */
+
     struct PhoneInfo {
         uint16 countryOfPhoneNumber;
         bytes32 encryptedPhoneNumber;
     }
+
+    /**
+     * @dev Variables:
+     * numberOfRegisteredAddresses : Quantity of users
+     * numberOfCountriesSupported : Quantity of countries which have a CountryInfo Contract associated
+     * s_PhoneNumbers : Information of the phones releated to the addresses
+     * s_CountryInfoContracts : Addresses of the CountryInfo Contracts 
+     */
 
     uint256 private numberOfRegisteredAddresses;
     uint16 private numberOfCountriesSupported;
 
     mapping(address user => PhoneInfo phoneInfo) s_PhoneNumbers;
     mapping(uint16 countryId => address countryInfoAddress) s_CountryInfoContracts;
+
+    /**
+     * @dev Function that saves the information of the Phone related to an address
+     * @param phoneNumberCountry_ : country where the phone is 
+     * @param encryptedPhoneNumber_ : Phone number that will be linked to that address
+     */
 
     function registerAccount(uint16 phoneNumberCountry_, bytes32 encryptedPhoneNumber_) external {
         bytes32 zero;
@@ -39,6 +58,14 @@ contract CountryInfoFactory is Ownable {
         s_PhoneNumbers[msg.sender] = aux;
         numberOfRegisteredAddresses++;
     }
+
+    /**
+     * @dev Function that creates new CountryInfo Contracts
+     * @param countryId_ : Id of the new country 
+     * @param decimals_  : Decimals that will be used 
+     * @param feeOnCancel_ : Fee that will be deducted if a plan is cancelled
+     * @param feeOnCancelMultiplier_ : Multiplier in case the plan is cancelled midway
+     */
 
     function createNewCountryInfo(
         uint16 countryId_,
@@ -55,6 +82,12 @@ contract CountryInfoFactory is Ownable {
         s_CountryInfoContracts[countryId_] = address(newCountry);
         numberOfCountriesSupported++;
     }
+
+    /**
+     * @dev Change the phone Number of an address
+     * @param countryId_ : country where the phone is 
+     * @param encryptedPhoneNumber_ : Phone number that will be linked to that address
+     */
 
     function changePhoneNumberOfAddress(bytes32 encryptedPhoneNumber_, uint16 countryId_) external {
         bytes32 zero;
